ff9e375c782a382af539154a328632072337bfb3
GreatHayat <greathayat@gmail.com>
2021-03-14 17:31:03 +0500

    setup cors

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const users = require("./routes/users");
@@ -6,6 +7,7 @@ const auth = require("./routes/auth");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 
